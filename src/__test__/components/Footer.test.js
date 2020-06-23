fb1dce8ac38d22fc118f73775459aa95c1a66ee8
Erickowski <daniel_perez_03081995@hotmail.com>
2020-06-23 12:33:52 -0500

    Intro a SnapShots

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -11,3 +12,10 @@ describe('<Footer />', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
+
+describe('Footer snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
+  })
+})
